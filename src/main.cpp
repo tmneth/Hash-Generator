bc5726b04ecc6b3624a422baab79df7ebd600d52
tmneth <80415416+tmneth@users.noreply.github.com>
2022-09-28 19:20:02 +0300

    Allow to parse file name as a command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -3,47 +3,58 @@
 #include "../include/hash.h"
 #include "../include/analysis.h"
 
-int main() {
-    Hash hash;
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[]) {
+
+    MYSHA mysha;
     int userChoice;
     std::string inputStr, filename, fileContents;
-    std::cout << "\nYou want to: \n"
-                 "1. Hash a string\n"
-                 "2. Hash a file\n"
-                 "3  Compare execution time to sha256, md5, sha1, keccak\n"
-                 "4  Test on determinism\n"
-                 "5  Test on collisions\n"
-                 "6. Test on Avalanche effect\n";
 
-    std::cout << std::string(50, '-') << std::endl;
-    std::cout << "Your choice: ";
-    std::cin >> userChoice;
+    cout << "You want to: " << endl;
+    cout << "1. Hash a string" << endl;
+    cout << "2. Hash a file" << endl;
+    cout << "3. I/O test" << endl;
+    cout << "4. Measure time complexity" << endl;
+    cout << "5. Test on collisions" << endl;
+    cout << "6. Test on Avalanche effect" << endl;
+    cout << "7. Compare hashing time to sha256, md5, sha1, keccak" << endl;
+
+    cout << std::string(50, '-') << std::endl;
+    cout << "Your choice: ";
+    cin >> userChoice;
     validateInput(userChoice, 1, 7);
 
     switch (userChoice) {
         case 1:
-            std::cout << "Enter a string: ";
+            cout << "Enter a string: \n";
             std::cin.ignore(1000, '\n');
-            getline(std::cin, inputStr);
-            std::cout << hash(inputStr) << std::endl;
+            getline(cin, inputStr);
+            cout << mysha(inputStr) << endl;
             break;
         case 2:
-            std::cout << "Enter a filename: ";
-            std::cin >> filename;
-            fileContents = readFileIntoStr(filename);
-            std::cout << hash(fileContents) << std::endl;
+            if (argc < 2) {
+                std::cerr << "No valid input file was found." << endl;
+            } else {
+                fileContents = readFileIntoStr(argv[1]);
+                cout << mysha(fileContents) << endl;
+            }
             break;
         case 3:
-            efficiencyTest();
+            specificationTest();
             break;
         case 4:
-            specificationTest();
+            hashTimeTest();
             break;
         case 5:
             collisionTest();
             break;
         case 6:
-            avalancheEffectComparison();
+            similarityTestComp();
+            break;
+        case 7:
+            hashTimeComp();
             break;
     }
     return 0;
